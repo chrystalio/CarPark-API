d17d09ee77e072f8317a20f6122b1e0261319b2d
chrystalio <chrystalio.k@outlook.com>
2023-04-30 06:27:25 +0700

    Set route apiResouces Vehicle

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\VehicleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Api\V1\Auth;
@@ -20,6 +21,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('profile', [Auth\ProfileController::class, 'update']);
     Route::put('password', Auth\PasswordUpdateController::class);
     Route::post('auth/logout', Auth\LogoutController::class);
+
+    Route::apiResource('vehicles', VehicleController::class);
 });
 
 Route::post('auth/register', Auth\RegisterController::class);
