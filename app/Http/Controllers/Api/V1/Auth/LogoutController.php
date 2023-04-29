ed2aadb57f1d7f6a76e3e2652666c512aaf9cc79
chrystalio <chrystalio.k@outlook.com>
2023-04-30 06:16:54 +0700

    Set invoke method to invalidate access token user

--- app/Http/Controllers/Api/V1/Auth/LogoutController.php
+++ app/Http/Controllers/Api/V1/Auth/LogoutController.php
@@ -7,5 +7,9 @@ use Illuminate\Http\Request;
 
 class LogoutController extends Controller
 {
-    //
+    public function __invoke(Request $request){
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->noContent();
+    }
 }
