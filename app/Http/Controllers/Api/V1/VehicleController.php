b41a74b1caf911e7010b41674c9eb64e545b361a
chrystalio <chrystalio.k@outlook.com>
2023-04-30 06:28:17 +0700

    Set resources methods, and implement the Resource & Request validation

--- app/Http/Controllers/Api/V1/VehicleController.php
+++ app/Http/Controllers/Api/V1/VehicleController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreVehicleRequest;
+use App\Http\Resources\VehicleResource;
 use App\Models\Vehicle;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class VehicleController extends Controller
 {
@@ -13,15 +17,16 @@ class VehicleController extends Controller
      */
     public function index()
     {
-        //
+        return VehicleResource::collection(Vehicle::all());
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreVehicleRequest $request)
     {
-        //
+        $vehicle = Vehicle::create($request->validated());
+        return VehicleResource::make($vehicle);
     }
 
     /**
@@ -29,22 +34,33 @@ class VehicleController extends Controller
      */
     public function show(Vehicle $vehicle)
     {
-        //
+        return VehicleResource::make($vehicle);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Vehicle $vehicle)
+    public function update(StoreVehicleRequest $request, Vehicle $vehicle)
     {
-        //
+        $vehicle->update($request->validated());
+
+        return response()->json(VehicleResource::make($vehicle), ResponseAlias::HTTP_ACCEPTED);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Vehicle $vehicle)
+    public function destroy(Vehicle $vehicle): Response
     {
-        //
+        $vehicle->delete();
+
+        return response()->noContent();
     }
+
+    /*  -We don't need to specify response()->json(), Laravel will automatically transform the API resource or Eloquent Model result into JSON, if the client specifies the Accept: application/json header
+        -We use the VehicleResource in a few places - once to return a collection and three times for a single model
+        -We use $request->validated() because this is returned from the Form Request class
+        -We reuse the same StoreVehicleRequest in this case because validation rules are identical for store and update
+        -We don't return anything from the destroy() method because, well, there's nothing to return if there's no vehicle anymore, right?
+    */
 }
