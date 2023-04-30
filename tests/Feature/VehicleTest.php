1e7b837899e4934bf9c5d3311faa1aeff39f639a
chrystalio <chrystalio.k@outlook.com>
2023-04-30 22:23:31 +0700

    Add test case user can get their own vehicles

--- tests/Feature/VehicleTest.php
+++ tests/Feature/VehicleTest.php
@@ -2,19 +2,34 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use App\Models\Vehicle;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class VehicleTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function testUserCanGetTheirOwnVehicles()
+    {
+        $john = User::factory()->create();
+        $vehicleForJohn = Vehicle::factory()->create([
+            'user_id' => $john->id
+        ]);
+
+        $adam = User::factory()->create();
+        $vehicleForAdam = Vehicle::factory()->create([
+            'user_id' => $adam->id
+        ]);
+
+        $response = $this->actingAs($john)->getJson('/api/v1/vehicles');
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['data'])
+            ->assertJsonCount(1, 'data')
+            ->assertJsonPath('data.0.plate_number', $vehicleForJohn->plate_number)
+            ->assertJsonMissing($vehicleForAdam->toArray());
     }
 }
