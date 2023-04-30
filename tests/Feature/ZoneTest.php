95f7c9505f7f81b412f6d530d50ca50ab23021a8
chrystalio <chrystalio.k@outlook.com>
2023-04-30 22:19:40 +0700

    Add testcase public user can get all zones

--- tests/Feature/ZoneTest.php
+++ tests/Feature/ZoneTest.php
@@ -8,13 +8,20 @@ use Tests\TestCase;
 
 class ZoneTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function testPublicUserCanGetAllZones()
+    {
+        $response = $this->getJson('/api/v1/zones');
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['data'])
+            ->assertJsonCount(3, 'data')
+            ->assertJsonStructure(['data' => [
+                ['*' => 'id', 'name', 'price_per_hour'],
+            ]])
+            ->assertJsonPath('data.0.id', 1)
+            ->assertJsonPath('data.0.name', 'Zone 1')
+            ->assertJsonPath('data.0.price_per_hour', 100);
     }
 }
